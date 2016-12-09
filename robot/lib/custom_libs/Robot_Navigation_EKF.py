3bcaa86be34db2ea91896f2565fb50f60810f589
Venefix <stephen2132@gmail.com>
2016-12-09 15:02:34 -0500

    Navigation EKF
    Added

--- robot/lib/custom_libs/Robot_Navigation_EKF.py
+++ robot/lib/custom_libs/Robot_Navigation_EKF.py
@@ -1,56 +1,74 @@
 import sympy
-import filterpy
-from sympy import symbols, Matrix
+# from sympy import symbols, Matrix
+# uncomment this
+# Titles of functions are BELOW their respective function to avoid PEP8 complaints
 
-class Robot_Navigation_EKF:
-    def __init__(self, loc):
-        self.EKF = filterpy.kalman.ExtendedKalmanFilter(dim_x = 5, dim_y = 5)
-        self.EKF.x = np.array()
 
-    def update(self, z, theta):
-        return self.EKF.update(z, HJacob, Hx_fun, )
+def state_trans_xjacobian(del_D, del_V, del_theta, theta):
+    Fx = Matrix([[1, 0, 0, 0, -del_D * sympy.sin(theta + del_theta)],
+                [0, 1, 0, 0, del_D * sympy.cos(theta + del_theta)],
+                [0, 0, 0, 0, -del_V * sympy.sin(theta + del_theta)],
+                [0, 0, 0, 0, del_V * sympy.cos(theta + del_theta)],
+                [0, 0, 0, 0, 1]])
+    return Fx
+# Jacobian of the State Transition Matrix with respect to X (state)
 
-    def predict_update(self, z, theta):
-        self.EKF.predict_update(z, stat)
 
-def state_trans_xjacob(del_D, del_V, del_theta, theta):
-    F = Matrix([[1, 0, 0, 0, -del_D * sympy.sin(theta + del_theta)],
-        [0, 1, 0, 0, del_D * sympy.cos(theta + del_theta)],
-        [0, 0, 0, 0, -del_V * sympy.sin(theta + del_theta)],
-        [0, 0, 0, 0, del_V * sympy.cos(theta + del_theta)],
-        [0, 0, 0, 0, 1]])
-    return F
+def state_trans_ujacobian(del_D, del_V, del_theta, theta):
+    Fu = Matrix([[-del_D * sympy.sin(theta + del_theta), 0, sympy.cos(theta + del_theta)],
+                [del_D * sympy.cos(theta + del_theta), 0, sympy.sin(theta + del_theta)],
+                [-del_V * sympy.sin(theta + del_theta), sympy.cos(theta + del_theta), 0],
+                [del_V * sympy.cos(theta + del_theta), sympy.sin(theta + del_theta), 0],
+                [1, 0, 0]])
+    return Fu
+# Jacobian of the State Transition Matrix with respect to u (controller)
 
-def state_trans_ujacob(del_D, del_V, del_theta, theta):
-    B = Matrix([[-del_D * sympy.sin(theta + del_theta), 0, sympy.cos(theta + del_theta)],
-        [del_D * sympy.cos(theta + del_theta), 0, sympy.sin(theta + del_theta)],
-        [-del_V * sympy.sin(theta + del_theta), sympy.cos(theta + del_theta), 0],
-        [del_V * sympy.cos(theta + del_theta), sympy.sin(theta + del_theta), 0],
-        [1, 0, 0]])
-    return B
 
-def proc_noise_covar(std_theta, std_V, std_D):
-    P = Matrix([[std_theta ** 2, 0, 0],
-        [0, std_V ** 2, 0],
-        [0, 0, std_D ** 2]])
+def proc_noise_covar(std_theta, std_V, std_D, Fu):
+    M = Matrix([[std_theta ** 2, 0, 0],
+                [0, std_V ** 2, 0],
+                [0, 0, std_D ** 2]])
+    Q = (Fu.dot(M)).dot(Transpose(Fu))  # I'm not sure if I can do this, maybe an error
+    return Q
+# Process Noise Covariance Matrix
+
+
+def proc_error_covar(Fx,Q,P_old):
+    P_temp = (Fx.dot(P_old)).dot(Transpose(Fx))
+    P = MatAdd(P_temp, Q)
     return P
+# Process Error Covariance Matrix
 
 def meas_noise_covar(std_ad, std_av, std_ag):
     R = Matrix([[std_ad ** 2, 0, 0, 0, 0],
-        [0, std_ad ** 2, 0, 0, 0],
-        [0, 0, std_av ** 2, 0, 0],
-        [0, 0, 0, std_av ** 2, 0],
-        [0, 0, 0, 0, std_g ** 2]])
+                [0, std_ad ** 2, 0, 0, 0],
+                [0, 0, std_av ** 2, 0, 0],
+                [0, 0, 0, std_av ** 2, 0],
+                [0, 0, 0, 0, std_ag ** 2]])
     return R
+# Measurement Noise Covariance Matrix
+
 
 def HJacob(dt):
     HJacobobian = Matrix([[dt ** (-2), 0, 0, 0, 0],
-         [0, dt ** (-2), 0, 0, 0],
-         [0, 0, dt ** (-1), 0, 0],
-         [0, 0, 0, dt ** (-1), 0],
-         [0, 0, 0, 0, dt ** (-1)]])
+                 [0, dt ** (-2), 0, 0, 0],
+                 [0, 0, dt ** (-1), 0, 0],
+                 [0, 0, 0, dt ** (-1), 0],
+                 [0, 0, 0, 0, dt ** (-1)]])
     return HJacobian
+# Jacobian of the Expected Measurement Function with respect to X (state)
 
-def Hx_fun(HJacob,x_est):
-    Hx = Hjacob.dot(x_est)
-    return Hx
+
+def h_funk(HJacobian,x_est):
+    h = Hjacobian.dot(x_est)
+    return h
+# Expected Measurement Function (as a function of its own Jacobian (O.O)  )
+
+def KalmanFilter(HJacobian, P, R, x_hat_old, z, h):
+    S_temp = (HJacobian.dot(P)).dot(Transpose(HJacobian))
+    S = MatAdd(S_temp, R)                        # Innovation Matrix
+    K = (P.dot(HJacobian)).dot(Inverse(S))       # Kalman Gain
+    Temp_for_P = -((K.dot(S)).(Transpose(K)))    # Temporary Value for next calculation - Not sure if I can use the negative sign like I did, check that?
+    P_next = MatAdd(P, Temp_for_P)               # P_old fot the next pass
+    x_hat = x_hat_old + K.dot(z-h)
+    return P_next, x_hat
