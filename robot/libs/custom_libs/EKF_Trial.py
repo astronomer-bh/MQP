ec57d30cd82d9275bdddf5087d347e514e1d6db5
Xaegrek <Xaegrek@gmail.com>
2017-06-12 10:52:33 -0400

    more print statements >.> hopefully we can get back to actually improving the filter

--- robot/libs/custom_libs/EKF_Trial.py
+++ robot/libs/custom_libs/EKF_Trial.py
@@ -4,7 +4,7 @@ import sympy
 from sympy import symbols, Matrix
 
 
-class RobotNavigatinoEKF:
+class RobotNavigationEKF:
 	def __init__(self, stdTheta, stdD, stdAD, stdAG):
 		# todo standard deviations of ??, probably measuremens or certainty in model
 		self.stdTheta = stdTheta
@@ -31,10 +31,10 @@ class RobotNavigatinoEKF:
 	# C matrix
 	def C_Matrix(self, dt):
 		C_Matrix = sympy.Matrix([
-			[sympy.cos(self.estX[2]) * 1 / (dt ** [2]), sympy.sin(self.estX[2]) * 1 / (dt ** [2]),
-			 (- self.estX[0] * sympy.sin(self.estX[2]) + sympy.cos(self.estX[2]) * self.estX[1]) * 1 / (dt ** [2])],
-			[-sympy.sin(self.estX[2]) * 1 / (dt ** [2]), sympy.cos(self.estX[2]) * 1 / (dt ** [2]),
-			 (- self.estX[0] * sympy.sin(self.estX[2]) - sympy.cos(self.estX[2]) * self.estX[1]) * 1 / (dt ** [2])],
+			[sympy.cos(self.estX[2]) * 1 / (dt ** (2)), sympy.sin(self.estX[2]) * 1 / (dt ** (2)),
+			 (- self.estX[0] * sympy.sin(self.estX[2]) + sympy.cos(self.estX[2]) * self.estX[1]) * 1 / (dt ** (2))],
+			[-sympy.sin(self.estX[2]) * 1 / (dt ** (2)), sympy.cos(self.estX[2]) * 1 / (dt ** (2)),
+			 (- self.estX[0] * sympy.sin(self.estX[2]) - sympy.cos(self.estX[2]) * self.estX[1]) * 1 / (dt ** (2))],
 			[0, 0, (1 / dt)]
 		])
 		return C_Matrix
@@ -51,6 +51,7 @@ class RobotNavigatinoEKF:
 			[0, 0, 1]
 		])
 		return Fx
+
 	def stateTransUJacob(self, u):
 		theta = self.estX[2]
 		delD = u[0]
@@ -72,8 +73,10 @@ class RobotNavigatinoEKF:
 	# process error covariance matrix
 	def procErrorCovar(self, u):
 		Fx = self.stateTransXJacob(u)
+		print("state trans x Jacobian matrix:", Fx)
 		Q = self.procNoiseCovar(u)
-		temP = Fx * self.P * Fx.T	#todo what does this do transposing
+		print("process noise covariance Q:", Q)
+		temP = Fx * self.P * sympy.Transpose(Fx)	#todo what does this do transposing (old  Fx.T)
 		if temP == 0 and Q == 0:
 			self.P = sympy.zeros(3)
 		elif temP == 0:
@@ -98,18 +101,27 @@ class RobotNavigatinoEKF:
 		expectedZfcn = self.C_Matrix(dt) * self.estX
 		return expectedZfcn
 
-
 	# main kalman function, all preceding feeds into this
 	# u = [delD, delTheta]
 	def KalmanFilter(self, z, u, dt):
+		print("measurement matrix z:", z)
+		print("control matrix u:", u)
+		print("time seperation dt:", dt)
 		Hjacobian = self.C_Matrix(dt)
+		print("C_matrix :", Hjacobian)
 		self.procErrorCovar(u)
+		print("process error covariance matrix P:", self.P)
 		#todo what is S
-		S = Hjacobian * self.P * Hjacobian.transpose() + self.R	#todo transposing
+		S = Hjacobian * self.P * sympy.Transpose(Hjacobian) + self.R	#todo transposing
+		print("S matrix:", S)
 		K = self.P * Hjacobian * S.inv()
+		print("kalman gain:", K)
 		#update P
-		self.P = self.P - K * S * K.transpose() #todo transposing
+		self.P = self.P - K * S * sympy.Transpose(K) #todo transposing (old K.transpose())
+		print("new P matrix:", self.P)
 		self.updateEstX(u)
 		h = self.expectedZfcn(dt)
+		print("new measurement function predicition:", h)
 		self.estX = self.estX + K * (z - h)
+		print("final X estimate:", self.estX)
 		return self.estX
